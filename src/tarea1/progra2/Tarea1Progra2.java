3cfaa648f9c03da2873e118d50976c7da10d9b4e
Derek <ds1517920@gmail.com>
2026-06-08 15:21:07 -0600

    Se implementa la captura de datos del usuario

--- src/tarea1/progra2/Tarea1Progra2.java
+++ src/tarea1/progra2/Tarea1Progra2.java
@@ -1,9 +1,17 @@
 package tarea1.progra2;
 
+import javax.swing.JOptionPane;
+
 public class Tarea1Progra2 {
 
     public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Bienvenido, por favor, inserte los datos que se le solicitan a continuación");
         
+        String nombre = JOptionPane.showInputDialog(null, "Nombre: ");
+        String apellido = JOptionPane.showInputDialog(null, "Apellido: ");
+        String edad = JOptionPane.showInputDialog(null, "Edad: "); 
+        String email = JOptionPane.showInputDialog(null, "Email: ");
+        String telefono = JOptionPane.showInputDialog(null, "Número de teléfono: ");
     }
     
 }
